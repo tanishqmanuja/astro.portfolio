f6268a802461ec98709415b76e047248e3d4b320
tanishqmanuja <tanishqmanuja@gmail.com>
2024-11-09 17:24:57 +0530

    ♻️ refactor: better site.config

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,4 +1,62 @@
+import type { Link } from "./types";
+
 export const URL = "https://tqman.pages.dev";
 export const TITLE = `Tanishq Manuja | Portfolio`;
 export const AUTHOR = "Tanishq Manuja";
 export const DESCRIPTION = "Portfolio of Tanishq Manuja";
+
+export const SKILLS = [
+  "Javascript",
+  "Typescript",
+  "HTML",
+  "CSS",
+  "C",
+  "C++",
+  "Angular",
+  "React",
+  "Astro",
+  "Node",
+  "Bun",
+  "Express",
+  "Ionic",
+  "Capacitor",
+  "MySQL",
+  "SQLite",
+  "Git",
+  "Docker",
+  "Linux",
+  "Bash",
+];
+
+export const SOCIALS: Link[] = [
+  {
+    name: "GitHub",
+    href: "https://github.com/tanishqmanuja",
+    title: `${AUTHOR} on GitHub`,
+  },
+  {
+    name: "X/Twitter",
+    href: "https://x.com/tanishqmanuja",
+    title: `${AUTHOR} on Twitter`,
+  },
+  {
+    name: "BlueSky",
+    href: "https://tanishqmanuja.bsky.social",
+    title: `${AUTHOR} on Bluesky`,
+  },
+  {
+    name: "Mail",
+    href: "mailto:tanishqmanuja@gmail.com",
+    title: `Send an email to ${AUTHOR}`,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/tanishqmanuja/",
+    title: `${AUTHOR} on LinkedIn`,
+  },
+  {
+    name: "Discord",
+    href: "https://discordapp.com/users/534785364926136323",
+    title: `${AUTHOR} on Discord`,
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type Link = {
+  name: string;
+  href: string;
+  title: string;
+};
