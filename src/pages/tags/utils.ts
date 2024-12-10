598a587356c64e5179b91139e35e6aaa07497dda
tanishqmanuja <tanishqmanuja@gmail.com>
2024-12-10 21:08:03 +0530

    ♻️ refactor: tags logic

--- src/pages/tags/utils.ts
+++ src/pages/tags/utils.ts
@@ -1,13 +1,29 @@
 import type { CollectionEntry } from "astro:content";
 
+export type TagEntry = {
+  tag: string;
+  projects: CollectionEntry<"project">[];
+  count: number;
+};
+
 type Project = CollectionEntry<"project">;
 export function getTagsFromProjects(projects: Project[]) {
-  const tags = new Set<string>();
+  const entries: Record<string, TagEntry> = {};
 
   for (const project of projects) {
     for (const tag of project.data.tags) {
-      tags.add(tag);
+      if (!entries[tag]) {
+        entries[tag] = {
+          tag,
+          projects: [],
+          count: 0,
+        };
+      }
+
+      entries[tag].projects.push(project);
+      entries[tag].count += 1;
     }
   }
-  return [...tags];
+
+  return Object.values(entries);
 }
