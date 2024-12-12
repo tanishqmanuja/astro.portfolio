4322391d0d0cdf51bb562d3c9635a11336507314
tanishqmanuja <tanishqmanuja@gmail.com>
2024-12-12 16:19:34 +0530

    ✨ feat: open props as js import

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,6 @@
 const postcssJitProps = require("postcss-jit-props");
-const path = require("path");
+const OpenProps = require("open-props");
 
 module.exports = {
-  plugins: [
-    postcssJitProps({
-      files: [
-        path.resolve(__dirname, "node_modules/open-props/open-props.min.css"),
-      ],
-    }),
-  ],
+  plugins: [postcssJitProps(OpenProps)],
 };
