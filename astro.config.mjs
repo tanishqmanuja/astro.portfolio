8b3241c7561e2406e5cb43e9f9b9277a79d610c2
tanishqmanuja <tanishqmanuja@gmail.com>
2024-12-17 18:12:01 +0530

    ✨ feat: video component and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, envField } from "astro/config";
 
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import { URL } from "./src/site.config";
 
 export default defineConfig({
   site: URL,
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   env: {
     schema: {
       GITHUB_TOKEN: envField.string({
