e1a0d6a6f25a70ac81b57af3d4796fd04d3d9c6f
tanishqmanuja <tanishqmanuja@gmail.com>
2024-12-19 15:48:40 +0530

    ✨ wip: more changes

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,13 +1,15 @@
+import postcssGlobalData from "@csstools/postcss-global-data";
 import OpenProps from "open-props";
-import type { Plugin } from "postcss";
+import type { AcceptedPlugin } from "postcss";
+import postcssCustomMedia from "postcss-custom-media";
 import postcssJitProps from "postcss-jit-props";
-
-// import postcssUtopia from "postcss-utopia";
-
-const postcssUtopia = require("postcss-utopia");
+import postcssUtopia from "postcss-utopia";
 
 export default {
   plugins: [
+    postcssGlobalData({
+      files: ["./node_modules/open-props/media.min.css"],
+    }),
     postcssJitProps({
       ...OpenProps,
       layer: "openprops",
@@ -16,7 +18,8 @@ export default {
       minWidth: 320,
       maxWidth: 1080,
     }),
+    postcssCustomMedia(),
   ],
 } satisfies {
-  plugins: Plugin[];
+  plugins: AcceptedPlugin[];
 };
--- src/types/postcss-jit-props.d.ts
+++ src/types/postcss-jit-props.d.ts
@@ -1,1 +1,26 @@
-declare module "postcss-jit-props";
+declare module "postcss-jit-props" {
+  import { PluginCreator } from "postcss";
+
+  interface JITPropsOptions {
+    /**
+     * Wrap the entire rule using `@layer` syntax.
+     */
+    layer?: string;
+
+    /**
+     * Files to use for JIT properties, globbing allowed.
+     */
+    files?: string[];
+
+    /**
+     * Selector the props are pushed to instead of `:root`.
+     */
+    custom_selector?: string;
+
+    [key: string]: string | number;
+  }
+
+  const JITProps: PluginCreator<JITPropsOptions>;
+
+  export default JITProps;
+}
