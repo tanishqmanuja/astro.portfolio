fe1150d538862eaee4fd9a7be9457c34da6ee056
tanishqmanuja <tanishqmanuja@gmail.com>
2024-12-20 12:24:06 +0530

    🧱 chore: update postcss jit props typedef

--- src/types/postcss-jit-props.d.ts
+++ src/types/postcss-jit-props.d.ts
@@ -1,26 +1,27 @@
 declare module "postcss-jit-props" {
   import { PluginCreator } from "postcss";
 
-  interface JITPropsOptions {
+  export interface Options {
     /**
-     * Wrap the entire rule using `@layer` syntax.
+     * Wraps the entire rule inside `@layer` syntax.
      */
     layer?: string;
 
     /**
-     * Files to use for JIT properties, globbing allowed.
+     * Where search for css props, globbing allowed.
      */
     files?: string[];
 
     /**
-     * Selector the props are pushed to instead of `:root`.
+     * Selector where the props are pushed to instead of `:root`.
      */
     custom_selector?: string;
-
-    [key: string]: string | number;
   }
 
-  const JITProps: PluginCreator<JITPropsOptions>;
+  type Props = Record<string, string | number>;
 
-  export default JITProps;
+  export type PostcssJitPropsOptions = Options & Props;
+  declare const postcssJitProps: PluginCreator<PostcssJitPropsOptions>;
+
+  export default postcssJitProps;
 }
