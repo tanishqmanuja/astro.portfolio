afd669040d4f1f73d384170c200a82470872beb0
tanishqmanuja <tanishqmanuja@gmail.com>
2024-12-28 13:54:50 +0530

    ♻️ refactor: cached function

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,29 +1,45 @@
 import { createStorage, type StorageValue } from "unstorage";
 
-import { fsCacheDriver } from "./unstorage/driver/fs-cache";
+import { fsCacheDriver } from "@/lib/unstorage/driver/fs-cache";
 
 const CACHE_DIR = ".cache";
 
-export const cache = createStorage({
+type CacheValue = { value: StorageValue };
+export const cache = createStorage<CacheValue>({
   driver: fsCacheDriver({ base: CACHE_DIR, freshness: "1d" }),
 });
+
+type CachedOptions<T = unknown> = {
+  validator?: (value: unknown) => T;
+  bypass?: boolean | (() => boolean);
+};
+
+const DEAFAULT_OPTIONS = {
+  bypass: import.meta.env.PROD,
+} satisfies CachedOptions;
 
 export async function cached<T extends StorageValue>(
   key: string,
   fn: () => Promise<T>,
-  validator: (value: unknown) => boolean = (v) => v !== null,
+  options?: CachedOptions<T>,
 ): Promise<T> {
-  if (import.meta.env.PROD) {
+  const o = { ...DEAFAULT_OPTIONS, ...options };
+
+  const bypass = typeof o.bypass === "function" ? o.bypass() : o.bypass;
+  if (bypass) {
     return fn();
   }
 
   const cachedValue = await cache.getItem(key);
 
-  if (validator(cachedValue)) {
-    return cachedValue as T;
+  if (cachedValue !== null) {
+    if (o.validator) {
+      return o.validator(cachedValue.value);
+    }
+    return cachedValue.value as T;
   }
 
-  const newValue = await fn();
-  await cache.setItem(key, newValue);
-  return newValue;
+  const value = await fn();
+  await cache.setItem(key, { value });
+  return value;
 }
